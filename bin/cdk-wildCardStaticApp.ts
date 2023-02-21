f1faa0e6dd6817d976c3dc66ea0cf678b3839cd0
Nick Mandylas <nick.mandylas@gmail.com>
2023-02-21 16:34:06 +1100

    updated packages to cdkv2

--- bin/cdk-wildCardStaticApp.ts
+++ bin/cdk-wildCardStaticApp.ts
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
-import * as cdk from '@aws-cdk/core';
-import { wildCardStaticApp } from '../lib/wildCardStaticApp-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { wildCardStaticApp } from "../lib/wildCardStaticApp-stack";
 
 const app = new cdk.App();
-const envUS  = { 
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: 'us-east-1' // Note the region is hard coded to us-east-1 as this is the requiref region for CloudFront certificates
+const envUS = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: "us-east-1", // Note the region is hard coded to us-east-1 as this is the requiref region for CloudFront certificates
 };
 
-new wildCardStaticApp(app, 'wildCardStaticApp', { env: envUS, description: "Multi Domain Stack (qs-1s3jcin2m)"});
+new wildCardStaticApp(app, "wildCardStaticApp", {
+  env: envUS,
+  description: "Multi Domain Stack (qs-1s3jcin2m)",
+});
